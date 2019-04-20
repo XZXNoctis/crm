8f0a588027b83422af2057d551d43baf94985f44
XZXNoctis <46205221+XZXNoctis@users.noreply.github.com>
2019-04-20 14:46:17 +0800

    Add files via upload

---
+++ mobile/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="home">
+		<mt-header fixed title="客户管理系统">
+			
+				<mt-button icon="back" slot="left"></mt-button>
+			
+			<mt-button icon="more" slot="right"></mt-button>
+		</mt-header>
+	</div>
+</template>
+
+<script>
+	export default {
+
+		components: {}
+	}
+</script>
